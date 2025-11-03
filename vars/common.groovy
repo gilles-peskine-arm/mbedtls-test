f1bb5a5864a91155382c79f4058601faee836aee
Gilles Peskine <Gilles.Peskine@arm.com>
2025-11-03 18:31:13 +0100

    Specify repo/branch in logging of all.sh component collection
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- vars/common.groovy
+++ vars/common.groovy
@@ -310,7 +310,7 @@ List<BranchInfo> get_branch_information(Collection<String> tls_branches, Collect
                             ),
                             returnStdout: true
                         ).trim().split('\n')
-                        echo "all.sh components: ${all.join(" ")}"
+                        echo "All all.sh components in ${repo}/${branch}: ${all.join(" ")}"
                         return all.collectEntries { element ->
                             return [(element): null]
                         }
@@ -335,7 +335,7 @@ List<BranchInfo> get_branch_information(Collection<String> tls_branches, Collect
                                 ),
                                 returnStdout: true
                             ).trim().split('\n')
-                            echo "Available all.sh components on ${platform}: ${available.join(" ")}"
+                            echo "Available all.sh components in ${repo}/${branch} on ${platform}: ${available.join(" ")}"
                             return available.collectEntries { element ->
                                 return [(element): platform]
                             }
