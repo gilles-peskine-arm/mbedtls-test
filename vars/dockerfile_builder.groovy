d287b1a3693dbaab64eed007aadf01508dab57f7
Gilles Peskine <Gilles.Peskine@arm.com>
2026-08-10 16:32:22 +0200

    dockerfile-builder: Fix type confusion with boolean parameters
    `env.FOO` reads `FOO` as a string, which is true whenever it is
    non-empty. The `BUILD_xxx` parameters are boolean, so they are
    `"false"` when false.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- vars/dockerfile_builder.groovy
+++ vars/dockerfile_builder.groovy
@@ -2,22 +2,22 @@ def run_job() {
     timestamps {
         stage('dockerfile-builder') {
             def jobs = [:]
-            if (env.BUILD_UBUNTU_16_04_AMD64_DOCKER_IMAGE) {
+            if (params.BUILD_UBUNTU_16_04_AMD64_DOCKER_IMAGE) {
                 jobs += gen_jobs.gen_dockerfile_builder_job('ubuntu-16.04-amd64', true)
             }
-            if (env.BUILD_UBUNTU_18_04_AMD64_DOCKER_IMAGE) {
+            if (params.BUILD_UBUNTU_18_04_AMD64_DOCKER_IMAGE) {
                 jobs += gen_jobs.gen_dockerfile_builder_job('ubuntu-18.04-amd64', true)
             }
-            if (env.BUILD_UBUNTU_18_04_ARM64_DOCKER_IMAGE) {
+            if (params.BUILD_UBUNTU_18_04_ARM64_DOCKER_IMAGE) {
                 jobs += gen_jobs.gen_dockerfile_builder_job('ubuntu-18.04-arm64', true)
             }
-            if (env.BUILD_UBUNTU_24_04_AMD64_DOCKER_IMAGE) {
+            if (params.BUILD_UBUNTU_24_04_AMD64_DOCKER_IMAGE) {
                 jobs += gen_jobs.gen_dockerfile_builder_job('ubuntu-24.04-amd64', true)
             }
-            if (env.BUILD_UBUNTU_24_04_ARM64_DOCKER_IMAGE) {
+            if (params.BUILD_UBUNTU_24_04_ARM64_DOCKER_IMAGE) {
                 jobs += gen_jobs.gen_dockerfile_builder_job('ubuntu-24.04-arm64', true)
             }
-            if (env.BUILD_ARM_COMPILERS_AMD64_DOCKER_IMAGE) {
+            if (params.BUILD_ARM_COMPILERS_AMD64_DOCKER_IMAGE) {
                 jobs += gen_jobs.gen_dockerfile_builder_job('arm-compilers-amd64', true)
             }
             jobs.failFast = false
